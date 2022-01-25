f7d03acbf01c1ca9c9946c6d451d457af6e70006
Maksim <status187@yandex.ru>
2022-01-25 20:19:16 +0300

    json

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,42 +1,18 @@
 import React from "react";
 import styles from './users.module.css';
+import * as axios from 'axios';
+import userPhoto from '../../assets/images/default-avatar.jpg'
 
 let Users = (props) => {
 
     if (props.users.length === 0) {
-        props.setUsers([
-                {
-                    id: 1, photoUrl: 'https://pixelbox.ru/wp-content/uploads/2020/11/ava-maincraft-youtube-1.jpg',
-                    followed: false,
-                    fullName: 'Maksim',
-                    status: 'I am a boss',
-                    location: {
-                        city: 'Tver',
-                        country: 'Russia'
-                    }
-                },
-                {
-                    id: 1, photoUrl: 'https://yt3.ggpht.com/a/AATXAJwvrbzr262fDtWZU-rQ-5jpgvQtTWv0YjJaDgVP=s900-c-k-c0xffffffff-no-rj-mo',
-                    followed: true,
-                    fullName: 'Sasha',
-                    status: 'I am a boss too',
-                    location: {
-                        city: 'Rzhev',
-                        country: 'Russia'
-                    }
-                },
-                {
-                    id: 1, photoUrl: 'https://yt3.ggpht.com/ytc/AKedOLQr01L0gOssXNfn-q5Mr6kgloNva-_OrRLiDAm4Bw=s900-c-k-c0x00ffffff-no-rj',
-                    followed: false,
-                    fullName: 'Pasha',
-                    status: 'I am a boss too',
-                    location: {
-                        city: 'Mockow',
-                        country: 'Russia'
-                    }
-                },
-            ]
-        )
+
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            // debugger;
+            props.setUsers(response.data.items);
+        });
+
+
     }
 
     return <div>
@@ -44,7 +20,7 @@ let Users = (props) => {
             props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photoUrl} alt='avatar' className={styles.userPhoto}/>
+                        <img src={u.photos.small != null ? u.photos.small : userPhoto} alt='avatar' className={styles.userPhoto}/>
                     </div>
                     <div>
                         { u.followed
@@ -55,12 +31,12 @@ let Users = (props) => {
                 </span>
                 <span>
                     <span>
-                        <div>{u.fullName}</div>
+                        <div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
                     <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+                        <div>{'u.location.country'}</div>
+                        <div>{'u.location.city'}</div>
                     </span>
                 </span>
             </div>)
